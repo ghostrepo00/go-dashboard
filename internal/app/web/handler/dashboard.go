a40a0b23f55d5b808e0d3d234b6bbdd4c7ad1813
ronny <aaa@aaa.com>
2023-11-13 16:20:43 +0700

    test

--- internal/app/web/handler/dashboard.go
+++ internal/app/web/handler/dashboard.go
@@ -8,6 +8,6 @@ import (
 
 func ConfigureBookmarkRouter(router *gin.Engine) {
 	router.GET("", func(c *gin.Context) {
-		c.HTML(http.StatusOK, "index.html", gin.H{"title": "Dashboard", "value2": "value 2"})
+		c.HTML(http.StatusOK, "index.html", gin.H{"title": "Dashboardc", "value2": "value 2"})
 	})
 }
