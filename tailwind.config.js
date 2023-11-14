1c159b56951bd974879d77638be0a6870866bff0
ronny <aaa@aaa.com>
2023-11-14 21:14:04 +0700

    Adding tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  //content: ["./internal/handler/web/template/**/*.{html,js}"],
+  content: ["./internal/handler/web/template/**/*.html"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
